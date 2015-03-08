9d8e0bbc103347e155c83c832b1b1d1b2b85c8e3
morgolon <morgolon@users.noreply.github.com>
2015-03-08 10:55:25 -0500

    this commit reverted the files to the 0.2.5e community version

---
+++ txtsplit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# usage: txtsplit.py <input_file_name> <output_dir>
+
+import os
+import sys
+import re
+import io
+
+assert len(sys.argv) == 3, "usage:\ntxtsplit.py <input_file_name> <output_dir>"
+iname = str(sys.argv[1])
+odir = str(sys.argv[2])
+
+ifile = io.open(iname, 'rt', encoding='utf-8')
+
+
+oname = None
+firstline = ifile.readline().replace(u'\ufeff','')
+match = re.search(ur'^#\s(\w+)$', firstline)
+if match:
+    oname = os.path.join(odir, match.group(1))
+assert oname, "file is in the wrong format, must start with a location name"
+ofile = io.open(oname, 'w', encoding='utf-8')
+ofile.write(firstline)
+
+for line in ifile:
+    match = re.search(ur'^#\s(\w+)$', line)
+    if match:
+        ofile.close()
+        oname = os.path.join(odir, match.group(1))
+        ofile = io.open(oname, 'w', encoding='utf-8')
+    ofile.write(line)
+        
